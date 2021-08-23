e2d609b1aac26d3abe21a22353a20c68a3efb79d
BluABK <bluabk@bluabk.net>
2021-08-23 17:12:52 +0200

    Remove the Unit Test project due to weird missing assembly issues.

---
+++ Person.cs
@@ -0,0 +1,41 @@
+﻿namespace FamilyTree
+{
+    public class Person
+    {
+        private const int YearUnset = -1;
+        private const string NameUnset = "";
+
+        public int Id;
+        public string FirstName;
+        public string LastName;
+        public int BirthYear;
+        public int DeathYear;
+        public Person Mother;
+        public Person Father;
+
+        public Person()
+        {
+            // Empty constructor.
+            // Used when member values are set directly upon instantiation.
+        }
+
+        public Person(int id, string firstName = NameUnset, string lastName = NameUnset, int birthYear = YearUnset, int deathYear = YearUnset, Person father = null, Person mother = null)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            BirthYear = birthYear;
+            DeathYear = deathYear;
+            Mother = mother;
+            Father = father;
+
+        }
+
+        public string GetDescription()
+        {
+            string description = "";
+
+            return description;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 
-namespace Family_Tree
+namespace FamilyTree
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
         }
     }
