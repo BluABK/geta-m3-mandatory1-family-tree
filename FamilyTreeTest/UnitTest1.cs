c67a5875966e2ba667ce73e37778d91fbf90ccdb
BluABK <bluabk@bluabk.net>
2021-08-23 17:20:44 +0200

    Create a test.

--- FamilyTreeTest/UnitTest1.cs
+++ FamilyTreeTest/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FamilyTree;
 
 namespace FamilyTreeTest
 {
@@ -8,6 +9,21 @@ namespace FamilyTreeTest
         [TestMethod]
         public void TestMethod1()
         {
+            var p = new Person
+            {
+                Id = 17,
+                FirstName = "Ola",
+                LastName = "Nordmann",
+                BirthYear = 2000,
+                DeathYear = 3000,
+                Father = new Person() { Id = 23, FirstName = "Per" },
+                Mother = new Person() { Id = 29, FirstName = "Lise" },
+            };
+
+            var actualDescription = p.GetDescription();
+            var expectedDescription = "Ola Nordmann (Id=17) Født: 2000 Død: 3000 Far: Per (Id=23) Mor: Lise (Id=29)";
+
+            Assert.AreEqual(expectedDescription, actualDescription);
         }
     }
 }
