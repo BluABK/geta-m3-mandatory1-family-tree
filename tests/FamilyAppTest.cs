def5720efcbddd65107f170e3d4a76ca7135a49d
BluABK <bluabk@bluabk.net>
2021-08-24 18:57:10 +0200

    Added unit test TestPersonWithoutChildren that verifies that children are not printed if person has none.
    Also verifies that the children header doesn't show.

--- tests/FamilyAppTest.cs
+++ tests/FamilyAppTest.cs
@@ -27,12 +27,37 @@ namespace FamilyTreeTest
 
             // Act.
             var app =               new FamilyApp(sverreMagnus, ingridAlexandra, haakon);
+
             var actualResponse =    app.HandleCommand("vis 3");
             var expectedResponse =  "Haakon Magnus (Id=3) Født: 1973 Far: Harald (Id=6)\n"
                                  +  "  Barn:\n"
                                  +  "    Sverre Magnus (Id=1) Født: 2005\n"
                                  +  "    Ingrid Alexandra (Id=2) Født: 2004\n";
             
+            // Assert.
+            Assert.AreEqual(expectedResponse, actualResponse);
+        }
+
+        [TestMethod]
+        public void TestPersonWithoutChildren()
+        {
+            // Arrange.
+            var sverreMagnus = new Person { Id = 1, FirstName = "Sverre Magnus", BirthYear = 2005 };
+            var ingridAlexandra = new Person { Id = 2, FirstName = "Ingrid Alexandra", BirthYear = 2004 };
+            var haakon = new Person { Id = 3, FirstName = "Haakon Magnus", BirthYear = 1973 };
+            var harald = new Person { Id = 6, FirstName = "Harald", BirthYear = 1937 };
+
+            sverreMagnus.Father = haakon;
+            ingridAlexandra.Father = haakon;
+            haakon.Father = harald;
+
+
+            // Act.
+            var app = new FamilyApp(harald, sverreMagnus, ingridAlexandra, haakon);
+
+            var actualResponse = app.HandleCommand("vis 2");
+            var expectedResponse = "Ingrid Alexandra (Id=2) Født: 2004 Far: Haakon Magnus (Id=3)";
+
             // Assert.
             Assert.AreEqual(expectedResponse, actualResponse);
         }
